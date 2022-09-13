b2e88fbde18b7dbdae7ba1b2954ac3e1fd29398f
lucas-hood <mostafa.bsh@gmail.com>
2022-09-13 23:23:49 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import Account from "./routes/Account";
+import CoinPage from "./routes/CoinPage";
+import axios from "axios";
+import Footer from "./components/Footer";
+import NoMatch from "./routes/NoMatch";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home coins={coins} />} />
+        <Route path="signin" element={<Signin />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="account" element={<Account />} />
+        <Route path="coins/:coinId" element={<CoinPage />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+      <Footer />
+    </ThemeProvider>
   );
 }
 
